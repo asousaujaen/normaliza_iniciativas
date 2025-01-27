ea39f44492e95e8b8778ffea4e909efc8ee065fa
Kyle Felipe <kylefelipe@gmail.com>
2025-01-27 14:59:24 -0300

    refazendo repo

---
+++ normaliza_iniciativas/normalizer.py
@@ -0,0 +1,28 @@
+from unidecode import unidecode
+
+from re import sub
+
+SUBSTITUTIONS = r'[\(\.\)\'\n\-\\#;:/\[\]|]'
+
+
+def clear_string(data, substitute=''):
+    if not data:
+        return data
+    string = data.strip()
+    string = string.lower()
+    string = sub(SUBSTITUTIONS, substitute or '', string)
+    string = unidecode(string)
+    string = sub(' ', '_', string)
+    string = sub(r'_{2,}', '_', string)
+    if string[0].isnumeric():
+        string = string[1:]
+    return string
+
+
+def set_none(data):
+    def aply(item):
+        if item in ('', '#REF!'):
+            return None
+        return item
+    ds = [list(map(aply, line)) for line in data]
+    return [line for line in ds if line.count(None) < len(line)]
